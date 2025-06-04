377df53f5222e79df38a42721ad21366ce13da61
kenneth-u-19 <karamirezm@unah.hn>
2025-06-03 23:48:29 -0600

    inicio del ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+//--Dado un arreglo de objetos, agrúpelos por el valor de una propiedad específica.
+//Agrupar elementos por propiedad
+
+
+function agruparPorPropiedad(arreglo, propiedad){
+
+
+}
+
+ const datos = [
+ { categoria: 'fruta', nombre: 'manzana' },
+ { categoria: 'verdura', nombre: 'zanahoria' },
+ { categoria: 'fruta', nombre: 'banana' },
+ { categoria: 'verdura', nombre: 'lechuga' }
+ ]
+
+const resultado = agruparPorPropiedad();
+console.log(resultado);
