96f24ef41b26b0162bf91aa8d9e75cfc4fde5047
kenneth-u-19 <karamirezm@unah.hn>
2025-06-08 16:07:43 -0600

    ejercicio6 completado

---
+++ ejercicio6.js
@@ -0,0 +1,30 @@
+ const lista1 = ['manzana', 'pera', 'banana'];
+ const lista2 = ['pera', 'uva', 'manzana', 'sandía'];
+
+ function combinarListasSinRepetir(lista1, lista2){
+    let listaTemp = [];
+    let listaFinal = [];
+    let contador = 0;
+
+    listaTemp = lista1.concat(lista2);
+
+    for(valor1 of listaTemp){
+        contador = 0;
+        for(valor2 of listaFinal){
+            if(valor2 === valor1){
+                contador++;
+            }
+        }
+
+        if(contador === 0){
+            listaFinal.push(valor1);
+        }
+    }
+
+    return listaFinal;
+ }
+
+
+
+ const resultado = combinarListasSinRepetir(lista1, lista2);
+ console.log(resultado);
