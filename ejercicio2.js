c3cae71982dddc041938b486d765060a2c884094
kenneth-u-19 <karamirezm@unah.hn>
2025-06-08 18:55:55 -0600

    ejercicio que faltaba completado

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,9 +1,25 @@
 function obtenerUsuario(id){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            resolve('hola mundo');
+            const objeto = {
+                1: 'Juan',
+                2: 'Maria'
+            };
+
+            if(objeto[id]){
+                resolve({id: id, nombre: objeto[id]});
+            }
+            else{
+                reject('Nombre no encontrado');
+            }
         },2000)
     })
 }
 
-console.log(obtenerUsuario(1));
+obtenerUsuario(1).then(nombre1 =>{
+    return obtenerUsuario(2).then(nombre2 =>{
+        console.log(`Usuarios cargados: ${nombre1.nombre} y ${nombre2.nombre}`);
+    });
+}).catch(error => {
+    console.log("error", error);
+});
